f899e283a6b04b0d8218db1476005fd59c76b5fe
kevin <kevin@bwlohas.com>
2024-01-23 10:33:45 +0800

    init

---
+++ Pods/Target Support Files/Pods-BadmintonWeiya-BadmintonWeiyaUITests/Pods-BadmintonWeiya-BadmintonWeiyaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BadmintonWeiya_BadmintonWeiyaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BadmintonWeiya_BadmintonWeiyaUITests
+@end
