66a84bc8fa9414af44f0873e2a91f0e74a077935
kevin <kevin@bwlohas.com>
2024-01-23 11:29:53 +0800

    viewcontroller 架構完成

---
+++ BadmintonWeiya/viewcontroller/ActivityViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BadmintonWeiya
+//
+//  Created by 小胖 on 2024/1/23.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- BadmintonWeiya/viewcontroller/SetViewController.swift
+++ BadmintonWeiya/viewcontroller/SetViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SetViewController.swift
 //  BadmintonWeiya
 //
 //  Created by 小胖 on 2024/1/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SetViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
